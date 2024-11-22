d275614d89fb009f9ce13965d719436cabe9d630
GitHub 핸들명 <GitHub >
2024-11-23 04:54:52 +0900

    make table

--- mission_course/members/models.py
+++ mission_course/members/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Member(models.Model):
+    name = models.TextField()
+    email = models.TextField()
+    birth = models.DateField()
+    join_date = models.DateField()
