3318487e2787c2f32c0b5608ba9091dc3dcb5dd9
GitHub 핸들명 <GitHub >
2024-11-23 17:54:04 +0900

    제약 조건 추가

--- mission_course/members/models.py
+++ mission_course/members/models.py
@@ -3,7 +3,8 @@ from django.db import models
 # Create your models here.
 
 class Member(models.Model):
+    member_id = models.IntegerField(primary_key = True)
     name = models.TextField()
-    email = models.TextField()
-    birth = models.DateField()
+    email = models.TextField(unique=True)
+    birth = models.DateField(null=True)
     join_date = models.DateField()
