a2c089aab1f7f0cddcf8e95fad14ebad187929d3
GitHub 핸들명 <GitHub >
2024-11-23 18:03:16 +0900

    commit

--- mission_course/members/models.py
+++ mission_course/members/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Member(models.Model):
-    member_id = models.IntegerField(primary_key = True)
     name = models.TextField()
     email = models.TextField(unique=True)
     birth = models.DateField(null=True)
